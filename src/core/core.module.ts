00394c313c2781fd02ba0d7cf4e05134cf19cbce
Lorenzo Bonicelli <lorenzobonicelli@libero.it>
2018-11-08 23:25:27 +0100

    Fix icon import in core module. Fix imports in shelter component

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { TabsBaseModule } from './tabs-base/tabs-base.module';
 import { ImageViewModule } from './image-view/image-view.module';
 import { ImageFullModule } from './image-full/image-full.module';
-import { IconComponent } from './icon/icon.component';
+import { IconModule } from './icon/icon.module';
 
 const CORE_COMPONENT_MODULES = [
     TabsBaseModule,
     ImageViewModule,
     ImageFullModule,
-    IconComponent
+    IconModule
 ];
 
 @NgModule({
