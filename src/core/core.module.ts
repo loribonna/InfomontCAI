6bb28c56101d7fe852c6ddc28ee6a608e87edfbd
Lorenzo Bonicelli <lorenzobonicelli@libero.it>
2018-11-08 23:23:06 +0100

    Add bc-icons. Add icons to control image-full

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { TabsBaseModule } from './tabs-base/tabs-base.module';
 import { ImageViewModule } from './image-view/image-view.module';
 import { ImageFullModule } from './image-full/image-full.module';
+import { IconComponent } from './icon/icon.component';
+
 const CORE_COMPONENT_MODULES = [
     TabsBaseModule,
     ImageViewModule,
-    ImageFullModule
+    ImageFullModule,
+    IconComponent
 ];
 
 @NgModule({
--- src/core/image-full/image-full.module.ts
+++ src/core/image-full/image-full.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from "@angular/core"; 
 import { CommonModule } from "@angular/common";
 import { ImageFullComponent } from "./image-full.component";
+import { IconModule } from "../icon/icon.module";
 
 @NgModule({
   declarations: [ImageFullComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, IconModule],
   exports: [ImageFullComponent]
 })
 export class ImageFullModule {}
