8578e8bbeadc6ec89b7c01380c9c18512af616f1
Lorenzo Bonicelli <lorenzobonicelli@libero.it>
2018-11-11 17:23:43 +0100

    Updated shelter base component style. Updated cache service with self retrival for data from shelter service

--- src/app/shelter/images/images.component.ts
+++ src/app/shelter/images/images.component.ts
@@ -1,69 +1,24 @@
 import { Component, OnInit, Input, HostListener } from "@angular/core";
 import { IImageData, CacheService } from "src/app/cache.service";
-import { ShelterService, IImage } from "../shelter.service";
-import { mergeMap } from "rxjs/operators";
-import { Buffer } from "buffer";
 
 @Component({
   selector: "app-images",
   templateUrl: "./images.component.html",
-  styleUrls: ["./images.component.scss"],
-  providers: [ShelterService]
+  styleUrls: ["./images.component.scss"]
 })
 export class ImagesComponent implements OnInit {
-  @Input() shelterId: string;
   images: IImageData[] = [];
   downloading = false;
   _fullView = false;
 
   constructor(
-    private cache: CacheService,
-    private shelterService: ShelterService
-  ) { }
+    private cache: CacheService
+  ) {}
 
   ngOnInit() {
-    if (this.shelterId) {
-      this.downloading = true;
-      const cachedData = this.cache.getImages();
-      if (!cachedData) {
-        this.images = cachedData;
-      } else {
-        this.shelterService
-          .getImagesByShelter(this.shelterId)
-          .pipe(
-            mergeMap(images => images),
-            mergeMap(image => this.shelterService.getFile((<any>image)._id))
-          )
-          .forEach(async image => {
-            await this.loadImage(image);
-          })
-          .then(() => {
-            this.downloading = false;
-          });
-      }
-    }
-  }
-
-  loadImage(fileData: IImage): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-      const data = Buffer.from(fileData.data);
-      const blob = new Blob([data], { type: <string>fileData.contentType });
-      const reader = new FileReader();
-      reader.onload = e => {
-        const src = reader.result;
-        this.images.push({ file: fileData, url: src });
-      };
-      reader.onloadend = e => {
-        resolve();
-      };
-      reader.onerror = err => {
-        reject(err);
-      };
-      reader.onabort = e => {
-        reject(e);
-      };
-
-      reader.readAsDataURL(blob);
+    this.downloading = true;
+    this.cache.loadShelterImages().forEach(image => {
+      this.images.push(image);
     });
   }
 
