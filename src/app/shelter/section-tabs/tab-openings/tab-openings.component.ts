5dbeafc73b266103b27bbdc2e698eb4dba6bb5f0
Lorenzo Bonicelli <lorenzobonicelli@libero.it>
2018-12-07 22:41:02 +0100

    Updated opening view. Updated shelter view

--- src/app/shelter/section-tabs/tab-openings/tab-openings.component.ts
+++ src/app/shelter/section-tabs/tab-openings/tab-openings.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { CacheService } from 'src/app/cache.service';
-import { TabItemBase } from '../tab.base';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { CacheService } from "src/app/cache.service";
+import { TabItemBase } from "../tab.base";
+
+
 
 @Component({
-  selector: 'app-tab-openings',
-  templateUrl: './tab-openings.component.html',
-  styleUrls: ['./tab-openings.component.scss']
+    selector: "app-tab-openings",
+    templateUrl: "./tab-openings.component.html",
+    styleUrls: ["./tab-openings.component.scss"]
 })
 export class TabOpeningsComponent extends TabItemBase {
+    constructor(
+        protected route: ActivatedRoute,
+        protected cache: CacheService
+    ) {
+        super(route, cache);
 
-  constructor(protected route: ActivatedRoute, protected cache: CacheService) {
-    super(route, cache);
-  }
+        this._baseProperty = "openingTime";
+    }
+
+    getOpeningProp(opening, prop) {
+        return opening && opening[prop] ? opening[prop] : null;
+    }
+
+    checkOpeningDate(opening, prop) {
+        return opening && opening[prop];
+    }
 }
-
