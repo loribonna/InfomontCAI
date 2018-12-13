b023877e9bd2c20c8a03381704804985ee00c590
Lorenzo Bonicelli <lorenzobonicelli@libero.it>
2018-12-13 19:45:25 +0100

    Add base style in tabs. Fix addSectionToData null data reference

--- src/app/shelter/section-tabs/tab-geo/tab-geo.component.ts
+++ src/app/shelter/section-tabs/tab-geo/tab-geo.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
-import { TabItemBase } from '../tab.base';
-import { ActivatedRoute } from '@angular/router';
-import { CacheService } from 'src/app/cache.service';
+import { Component, ViewEncapsulation } from "@angular/core";
+import { TabItemBase } from "../tab.base";
+import { ActivatedRoute } from "@angular/router";
+import { CacheService } from "src/app/cache.service";
 
 @Component({
-  selector: 'app-tab-geo',
-  templateUrl: './tab-geo.component.html',
-  styleUrls: ['./tab-geo.component.scss']
+    selector: "app-tab-geo",
+    host: {
+        "[class.geo-container]": "true"
+    },
+    templateUrl: "./tab-geo.component.html",
+    styleUrls: ["./tab-geo.component.scss"],
+    encapsulation: ViewEncapsulation.None
 })
 export class TabGeoComponent extends TabItemBase {
-
-  constructor(protected route: ActivatedRoute, protected cache: CacheService) {
-    super(route, cache);
-    this._baseProperty = "location";
-  }
+    constructor(
+        protected route: ActivatedRoute,
+        protected cache: CacheService
+    ) {
+        super(route, cache);
+        this._baseProperty = "location";
+    }
 }
