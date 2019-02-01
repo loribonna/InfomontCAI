ed3e8cb22bbbea254a570c66318626a7e9257be1
Lorenzo Bonicelli <lorenzobonicelli@libero.it>
2019-02-01 13:41:54 +0100

    Updated index to serve app. Add dotenv as requirement

--- index.js
+++ index.js
@@ -3,24 +3,34 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// Serve only the static files form the dist directory
-app.get("/", function(req, res) {
-    res.redirect("/base")
-});
+if (process.env.DEBUG) {
+    app.get("/", function(req, res) {
+        res.redirect("/base");
+    });
 
-app.get("/base", function(req, res) {
-    res.sendFile(path.join(__dirname + "/main.html"));
-});
+    app.get("/base", function(req, res) {
+        res.sendFile(path.join(__dirname + "/main.html"));
+    });
 
-app.use(express.static(__dirname + "/dist/InfomontCAI"));
+    app.use(express.static(__dirname + "/dist/InfomontCAI"));
 
-app.get("/target", function(req, res){
-    res.sendFile(path.join(__dirname + "/dist/InfomontCAI/index.html"))
-})
+    app.get("/target", function(req, res) {
+        res.sendFile(path.join(__dirname + "/dist/InfomontCAI/index.html"));
+    });
 
-app.get("/:id", function(req, res) {
-    res.sendFile(path.join(__dirname + "/dist/InfomontCAI/index.html"))
-});
+    app.get("/:id", function(req, res) {
+        res.sendFile(path.join(__dirname + "/dist/InfomontCAI/index.html"));
+    });
+} else {
+    // Serve only the static files form the dist directory
+    app.use(express.static(__dirname + "/dist/InfomontCAI"));
+
+    app.get("/:id", function(req, res) {
+        res.sendFile(path.join(__dirname + "/dist/InfomontCAI/index.html"));
+    });
+}
 
 // Start the app by listening on the default Heroku port
-app.listen(4200);
+app.listen(process.env.PORT || 8090, function() {
+    console.log("Listening on port " + (process.env.PORT || 8090));
+});
